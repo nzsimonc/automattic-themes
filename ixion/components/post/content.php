72d0b401e236bc3d7d1cdfaf69caf11a4b89ea62
nzsimonc <6680265+nzsimonc@users.noreply.github.com>
2022-09-10 09:19:10 +1200

    Update content.php

--- ixion/components/post/content.php
+++ ixion/components/post/content.php
@@ -33,11 +33,7 @@
 		</header>
 		<div class="entry-content">
 			<?php
-				the_content( sprintf(
-					/* translators: %s: Name of current post. */
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'ixion' ), array( 'span' => array( 'class' => array() ) ) ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				) );
+				the_content();
 
 				wp_link_pages( array(
 					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'ixion' ),
